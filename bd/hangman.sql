602ae0f93b0a610efbe2629c0614aea7b185c3df
profesor <profesor@dawdistancia.net>
2026-05-18 19:43:19 +0200

    Update hangman.sql

--- bd/hangman.sql
+++ bd/hangman.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `hangman_26_2`
+-- Base de datos: `hangman_26_1`
 --
-CREATE DATABASE IF NOT EXISTS `hangman_26_2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;
-USE `hangman_26_2`;
+CREATE DATABASE IF NOT EXISTS `hangman_26_1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;
+USE `hangman_26_1`;
 
 -- --------------------------------------------------------
 
